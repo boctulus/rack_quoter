89abdac095453e645036bfb9943d64a6332a1355
Pablo Bozzolo <boctulus@gmail.com>
2023-09-24 14:59:50 +0800

    .

--- index.php
+++ index.php
@@ -94,16 +94,28 @@ add_action('wp_loaded', function() use ($cfg) {
 
 if (isset($_GET['credits'])){
     add_action('wp_footer', function(){ 
+		$app_name = config()['app_name'] ?? 'Plugin development';
         ?>
-            <div style="
+            <div id="dev-credits" style="
             height: 60px;
             text-align: center; margin: auto;
             width: 100%;
             padding-top: 20px;
             padding-bottom: 20px;
             background-color: #f0ad4e; ">
-            <strong>Rack Quoter Plugin</strong> by <b>Pablo Bozzolo</b> < boctulus@gmail.com >
+            <strong><?= $app_name ?></strong> by <b>Pablo Bozzolo</b> < boctulus@gmail.com >
             </div>
+
+			<script>
+				document.addEventListener('DOMContentLoaded', function() {
+					if (window.location.href.includes('credits')) {
+						var creditsElement = document.getElementById('dev-credits');
+						if (creditsElement) {
+							creditsElement.scrollIntoView({ behavior: 'smooth' });
+						}
+					}
+				});
+			</script>
         <?php    
     }); 
 }
